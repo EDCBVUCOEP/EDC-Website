f2470a9c9978d5b9ce23aa34fd6980087c145e71
Shikhar <shikarvaish@gmail.com>
2017-09-12 17:21:09 +0530

    changed config file

--- config.php
+++ config.php
@@ -7,7 +7,8 @@ $password = "";
 $conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error)
+{
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
